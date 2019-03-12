36015bf90c18a2705288127b7287ea6e1b05234f
paldepind <simonfv@gmail.com>
2019-03-12 20:15:50 +0100

    Add sink

--- src/Hareactive/Interop.js
+++ src/Hareactive/Interop.js
@@ -2,6 +2,16 @@ const H = require("@funkia/hareactive");
 
 exports._producerStream = H.producerStream;
 
+exports.sinkStream = H.sinkStream;
+
+exports._pushSink = function(a, sinkStream) {
+  sinkStream.push(a);
+}
+
+exports.sinkStreamToStream = function(sinkStream) {
+  return sinkStream;
+}
+
 exports._subscribe = function(cb, stream) {
   stream.subscribe(cb);
 }
--- src/Hareactive/Interop.purs
+++ src/Hareactive/Interop.purs
@@ -2,27 +2,43 @@
 
 module Hareactive.Interop
   ( subscribe
+  , producerStream
+  , SinkStream
+  , sinkStream
+  , sinkStream'
+  , sinkStreamToStream
+  , pushSink
   ) where
 
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Uncurried (EffectFn1, EffectFn2, mkEffectFn1, runEffectFn1, runEffectFn2)
 import Hareactive.Types (Behavior, Stream)
-import Prelude (Unit, (<<<))
+import Prelude (type (~>), Unit, (<$>), (<<<))
 
 type PushCallback a = a -> Effect Unit
 
 type ProducerFunction a = PushCallback a -> Effect (Effect Unit)
 
+-- Create a stream from an effectful function.
 producerStream :: forall a. ProducerFunction a -> Stream a
 producerStream f = _producerStream (mkEffectFn1 (\cb -> f (runEffectFn1 cb)))
 
 foreign import _producerStream :: forall a. (EffectFn1 (EffectFn1 a Unit) (Effect Unit)) -> Stream a
 
--- Executes the side-effect for each occurrence of the stream.
-subscribe :: forall a. (a -> Effect Unit) -> Stream a -> Effect Unit
-subscribe = runEffectFn2 _subscribe <<< mkEffectFn1
+foreign import data SinkStream :: Type -> Type
 
-foreign import _subscribe :: forall a. EffectFn2 (EffectFn1 a Unit) (Stream a) Unit
+foreign import sinkStream :: forall a. Effect (SinkStream a)
+
+sinkStream' :: forall a. Effect (Tuple (SinkStream a) (Stream a))
+sinkStream' = (\sink -> Tuple sink (sinkStreamToStream sink)) <$> sinkStream 
+
+pushSink :: forall a. a -> SinkStream a -> Effect Unit
+pushSink = runEffectFn2 _pushSink
+
+foreign import _pushSink :: forall a. EffectFn2 a (SinkStream a) Unit
+
+foreign import sinkStreamToStream :: SinkStream ~> Stream
 
 -- Creates a behavior from an effectful function.
 --
@@ -34,3 +50,9 @@ fromFunction :: forall a. (Number -> Effect a) -> Behavior a
 fromFunction = _fromFunction <<< mkEffectFn1
 
 foreign import _fromFunction :: forall a. (EffectFn1 Number a) -> Behavior a
+
+-- Executes the side-effect for each occurrence of the stream.
+subscribe :: forall a. (a -> Effect Unit) -> Stream a -> Effect Unit
+subscribe = runEffectFn2 _subscribe <<< mkEffectFn1
+
+foreign import _subscribe :: forall a. EffectFn2 (EffectFn1 a Unit) (Stream a) Unit
