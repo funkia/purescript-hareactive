8a028c573f6d3f629855be34f19a63fc68c292d2
paldepind <simonfv@gmail.com>
2019-03-12 20:15:59 +0100

    Setup unit testing and add small test

--- test/Main.purs
+++ test/Main.purs
@@ -2,8 +2,29 @@ module Test.Main where
 
 import Prelude
 
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
+import Effect.Class (liftEffect)
+import Effect.Ref as Ref
+import Hareactive.Interop (pushSink, sinkStream', sinkStreamToStream, subscribe)
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (run)
+
+addRef :: Int -> Ref.Ref Int -> Effect Unit
+addRef n ref = Ref.modify_ (n + _) ref
 
 main :: Effect Unit
-main = do
-  pure unit
+main = run [consoleReporter] do
+  describe "hareactive" do
+    it "sink and subscribe works" do
+      n <- liftEffect $ Ref.new 0
+      Tuple sink stream <- liftEffect $ sinkStream'
+      liftEffect $ subscribe (_ `addRef` n) stream
+      liftEffect $ pushSink 3 sink
+      liftEffect $ pushSink 2 sink
+      liftEffect $ pushSink 5 sink
+      result <- liftEffect $ Ref.read n
+      result `shouldEqual` 10
+      pure unit
